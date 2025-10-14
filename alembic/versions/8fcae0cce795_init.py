62d8a07f68100db4be839a69720ed7080ce6014b
peyro <peyrovskaaa@gmail.com>
2025-10-14 18:39:52 +0200

    add comments and image generation

--- alembic/versions/8fcae0cce795_init.py
+++ alembic/versions/8fcae0cce795_init.py
@@ -1,8 +1,8 @@
 """init
 
-Revision ID: 856e70ca9a07
+Revision ID: 8fcae0cce795
 Revises: 
-Create Date: 2025-09-17 18:43:32.185257
+Create Date: 2025-10-14 18:39:37.878595
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '856e70ca9a07'
+revision: str = '8fcae0cce795'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -26,14 +26,17 @@ def upgrade() -> None:
     sa.Column('user_uuid', sa.String(), nullable=False),
     sa.Column('site', sa.Enum('COPART', 'IAAI', name='auctionenum'), nullable=False),
     sa.Column('make', sa.String(), nullable=False),
-    sa.Column('model', sa.String(), nullable=False),
-    sa.Column('year_from', sa.Integer(), nullable=False),
-    sa.Column('year_to', sa.Integer(), nullable=False),
-    sa.Column('odo_from', sa.Integer(), nullable=False),
-    sa.Column('odo_to', sa.Integer(), nullable=False),
-    sa.Column('document', sa.String(), nullable=False),
-    sa.Column('transmission', sa.String(), nullable=False),
-    sa.Column('status', sa.String(), nullable=False),
+    sa.Column('model', sa.String(), nullable=True),
+    sa.Column('year_from', sa.Integer(), nullable=True),
+    sa.Column('year_to', sa.Integer(), nullable=True),
+    sa.Column('odo_from', sa.Integer(), nullable=True),
+    sa.Column('odo_to', sa.Integer(), nullable=True),
+    sa.Column('document', sa.String(), nullable=True),
+    sa.Column('transmission', sa.String(), nullable=True),
+    sa.Column('status', sa.String(), nullable=True),
+    sa.Column('auction_date_from', sa.DateTime(timezone=True), nullable=True),
+    sa.Column('auction_date_to', sa.DateTime(timezone=True), nullable=True),
+    sa.Column('stage', sa.Enum('FAILED', 'IN_PROGRESS', 'COMPLETED', name='requeststage'), nullable=True),
     sa.Column('created_at', sa.DateTime(timezone=True), nullable=False),
     sa.PrimaryKeyConstraint('id')
     )
@@ -43,6 +46,7 @@ def upgrade() -> None:
     sa.Column('auction', sa.Enum('COPART', 'IAAI', name='auctionenum'), nullable=False),
     sa.Column('title', sa.String(), nullable=False),
     sa.Column('odometer', sa.Integer(), nullable=False),
+    sa.Column('year', sa.Integer(), nullable=False),
     sa.Column('reserve_price', sa.Integer(), nullable=True),
     sa.Column('vin', sa.String(), nullable=False),
     sa.Column('status', sa.String(), nullable=False),
@@ -50,10 +54,12 @@ def upgrade() -> None:
     sa.Column('delivery_price', sa.Integer(), nullable=False),
     sa.Column('shipping_price', sa.Integer(), nullable=False),
     sa.Column('average_sell_price', sa.Integer(), nullable=True),
+    sa.Column('primary_damage', sa.String(), nullable=True),
     sa.Column('is_posted', sa.Boolean(), nullable=False),
     sa.Column('image_description', sa.String(), nullable=True),
     sa.Column('image_score', sa.Integer(), nullable=True),
     sa.Column('images', sa.String(), nullable=False),
+    sa.Column('comment', sa.String(), nullable=True),
     sa.Column('request_id', sa.Integer(), nullable=False),
     sa.Column('created_at', sa.DateTime(timezone=True), nullable=False),
     sa.ForeignKeyConstraint(['request_id'], ['request_filters.id'], ),
